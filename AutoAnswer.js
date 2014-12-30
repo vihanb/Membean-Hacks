5c26b338744ecb716d236b1b9e0e2659eca97222
Ben <benjamintyszka@gmail.com.com>
2014-12-30 15:19:35 -0500

    Version 1.1.1
    Added credits

--- AutoAnswer.js
+++ AutoAnswer.js
@@ -1,3 +1,10 @@
+/**DONT COPY LINES 1-6
+ *@author <h1 style="background-color:#000">Ben Tyszka<h1>
+ *@version 1.1.1
+ *@file AutoAnswer.js
+ *@desc Auto Answers membean.com questions!
+ *Copy everything from "if" down and paste it in the console per the instructions on the website
+ */
 if($("#wordform") == null){
 $("li.answer").click();
 console.log("MultiChoice");
