b2b5306432867f7994e3775beaa584ad5e3bd170
Ben <benjamintyszka@gmail.com.com>
2014-12-30 15:14:38 -0500

    Version 1.1
    Added auto detection!
    Added documentation!

--- AutoAnswer.js
+++ AutoAnswer.js
@@ -1,9 +1,7 @@
-/*
-	Copy this code and put in in the console and hit enter (click f12 and select the farthest tab to the right (Called console)
-	I am not responsible for any repercussions from your using of this code
-	This only works on those multiple choice ones
-Copy the line below this sentence*/
+if($("#wordform") == null){
 $("li.answer").click();
-
-/*This is for those pesky type in ones (This hasn't been tested on the fill in ones with the picture nor has it been tested on the fill in right after learning a new word) */
+console.log("MultiChoice");
+}else{
 $("#choice").val($("#full-answer").html().substring(1));$("#pass__pass").parent().submit();
+	console.log("Fill in");
+}
